901751a0eaade7b9a8d3e1431805f688ce872219
Jackk <Baihaqi070809@gmail.com>
2025-12-02 13:48:26 +0700

    challenge

--- src/config.js
+++ src/config.js
@@ -1,1 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
 export const API_BASE_URL = "https://ta-ppb-baihaqi-api.vercel.app/api";
+
+const supabaseUrl = "https://YOUR_PROJECT_ID.supabase.co"; 
+const supabaseKey = "YOUR_ANON_KEY"; 
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
